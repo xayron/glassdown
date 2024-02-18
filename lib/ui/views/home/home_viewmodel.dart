ea3ba422534cd809e485362e1bc0ed62a07499a0
sinneida <lukaszr23@gmail.com>
2024-02-18 02:18:34 +0100

    add welcome page for handling permissions

--- lib/ui/views/home/home_viewmodel.dart
+++ lib/ui/views/home/home_viewmodel.dart
@@ -3,22 +3,10 @@ import 'dart:async';
 import 'package:glass_down_v2/app/app.locator.dart';
 import 'package:glass_down_v2/services/settings_service.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:stacked/stacked.dart';
 
 class HomeViewModel extends StreamViewModel with IndexTrackingStateHelper {
   final _settings = locator<SettingsService>();
-
-  Future<void> handlePermissions() async {
-    await Permission.manageExternalStorage
-        .onPermanentlyDeniedCallback(() => openAppSettings())
-        .onDeniedCallback(() => openAppSettings())
-        .request();
-    await Permission.requestInstallPackages
-        .onPermanentlyDeniedCallback(() => openAppSettings())
-        .onDeniedCallback(() => openAppSettings())
-        .request();
-  }
 
   @override
   void onData(data) {
