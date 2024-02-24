abd386a995203b7a24f6c2d4bd468c4d9232659d
sinneida <lukaszr23@gmail.com>
2024-02-24 22:46:37 +0100

    allow to show updater when dev options is enabled

--- lib/ui/views/settings/items/check_updates/check_updates_model.dart
+++ lib/ui/views/settings/items/check_updates/check_updates_model.dart
@@ -1,19 +1,23 @@
 import 'package:flutter_logs/flutter_logs.dart';
 import 'package:glass_down_v2/app/app.locator.dart';
 import 'package:glass_down_v2/app/app.snackbar.dart';
+import 'package:glass_down_v2/services/settings_service.dart';
 import 'package:glass_down_v2/services/updater_service.dart';
 import 'package:glass_down_v2/ui/bottom_sheets/updater/update_sheet.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
-class CheckUpdatesModel extends BaseViewModel {
+class CheckUpdatesModel extends ReactiveViewModel {
   final _updater = locator<UpdaterService>();
   final _snackbar = locator<SnackbarService>();
+  final _settings = locator<SettingsService>();
+
+  bool get devOptions => _settings.devOptions;
 
   Future<void> checkUpdates() async {
     try {
       final result = await _updater.checkUpdates();
-      if (result) {
+      if (result || devOptions) {
         showUpdaterSheet();
       } else {
         _snackbar.showCustomSnackBar(
@@ -30,4 +34,7 @@ class CheckUpdatesModel extends BaseViewModel {
       );
     }
   }
+
+  @override
+  List<ListenableServiceMixin> get listenableServices => [_settings];
 }
