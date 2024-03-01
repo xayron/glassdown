ced2b8d80b974ba4a6b7d1924385b6c36f03a23e
sinneida <lukaszr23@gmail.com>
2024-03-01 18:05:18 +0100

    get precise function name

--- lib/util/function_name.dart
+++ lib/util/function_name.dart
@@ -1,22 +1,15 @@
 String getFunctionName() {
-  // Get the stack trace
   StackTrace stackTrace = StackTrace.current;
 
-  // Convert the stack trace to a string
   String stackTraceString = stackTrace.toString();
 
-  // Split the stack trace string by line breaks
   List<String> lines = stackTraceString.split('\n');
 
-  // The second line usually contains information about the current function
   if (lines.length > 1) {
-    // Extract the function name from the second line
-    String functionName = lines[1].trim();
+    String functionName = lines[1].split('.')[1].split(' ')[0];
 
-    // Return the function name
     return functionName;
   }
 
-  // Return a default value if the function name couldn't be extracted
   return 'Unknown Function';
 }
