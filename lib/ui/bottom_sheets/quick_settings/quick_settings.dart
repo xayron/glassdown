cf8ed3b412faba125b503c87319c3f597e0c0a23
sinneida <55593562+Sinneida@users.noreply.github.com>
2024-03-28 20:58:18 +0100

    add some apps tiles into quick settings

--- lib/ui/bottom_sheets/quick_settings/quick_settings.dart
+++ lib/ui/bottom_sheets/quick_settings/quick_settings.dart
@@ -4,9 +4,13 @@ import 'package:glass_down_v2/ui/common/ui_helpers.dart';
 import 'package:glass_down_v2/ui/views/settings/items/app_architecture/app_architecture.dart';
 import 'package:glass_down_v2/ui/views/settings/items/exclude_bundles/exclude_bundles.dart';
 import 'package:glass_down_v2/ui/views/settings/items/exclude_unstable/exclude_unstable.dart';
+import 'package:glass_down_v2/ui/views/settings/items/offer_deleting_old_apks/offer_deleting_old_apks.dart';
 import 'package:glass_down_v2/ui/views/settings/items/pages_count/pages_count.dart';
+import 'package:glass_down_v2/ui/widgets/settings/common/group_header.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
+
+import '../../views/settings/items/delete_old_versions/delete_old_versions.dart';
 
 Future<T?> showQuickSettingsSheet<T>() {
   return showModalBottomSheet(
@@ -36,29 +40,36 @@ Future<T?> showQuickSettingsSheet<T>() {
                   ),
                 ),
                 verticalSpaceMedium,
-                Material(
-                  type: MaterialType.card,
-                  surfaceTintColor: Theme.of(context).colorScheme.primary,
-                  borderRadius: BorderRadius.circular(16),
-                  elevation: 1,
-                  child: ListView(
-                    padding: const EdgeInsets.all(0),
-                    physics: const NeverScrollableScrollPhysics(),
-                    clipBehavior: Clip.antiAlias,
-                    shrinkWrap: true,
-                    children: const [
-                      ExcludeBundles(rounded: true),
-                      ExcludeUnstable(rounded: true),
-                      AppArchitecture(rounded: true),
-                      PagesCount(rounded: true),
-                    ],
+                Expanded(
+                  child: Material(
+                    type: MaterialType.card,
+                    surfaceTintColor: Theme.of(context).colorScheme.primary,
+                    borderRadius: BorderRadius.circular(16),
+                    elevation: 1,
+                    child: Scrollbar(
+                      interactive: true,
+                      child: SingleChildScrollView(
+                        child: ListView(
+                          padding: const EdgeInsets.all(0),
+                          physics: const NeverScrollableScrollPhysics(),
+                          clipBehavior: Clip.antiAlias,
+                          shrinkWrap: true,
+                          children: const [
+                            GroupHeader(name: 'Filters'),
+                            ExcludeBundles(rounded: true),
+                            ExcludeUnstable(rounded: true),
+                            AppArchitecture(rounded: true),
+                            PagesCount(rounded: true),
+                            GroupHeader(name: 'Apps'),
+                            DeleteOldVersions(),
+                            OfferDeletingOldApks(),
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-                const Expanded(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                  ),
-                ),
+                verticalSpaceMedium,
                 Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
