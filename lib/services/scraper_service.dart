21c160dcc2e9ecca7e89b424dd35bc623b890a7d
sinneida <55593562+Sinneida@users.noreply.github.com>
2024-06-14 22:52:53 +0200

    remove redundant page parsing and get url from full url

--- lib/services/scraper_service.dart
+++ lib/services/scraper_service.dart
@@ -166,31 +166,11 @@ class ScraperService with ListenableServiceMixin {
     }
   }
 
-  Future<VersionLink> getLinkFromAppSearch(SearchResult searchResult) async {
+  VersionLink getLinkFromAppSearch(SearchResult searchResult) {
     try {
-      final response = await _dio.get<String>(searchResult.link);
+      final url = searchResult.link.split('/')[3];
 
-      if (response.statusCode != 200) {
-        throw ScrapeError(
-          'HTTP Error: Failed to fetch search list',
-          response.statusMessage,
-        );
-      }
-
-      final document = parse(response.data);
-      final allLinks = document.getElementsByTagName('a');
-      final linkElement = allLinks.firstWhere(
-        (element) {
-          final href = element.attributes['href'];
-          if (href != null) {
-            return href.contains('appcategory');
-          }
-          return false;
-        },
-      );
-      final link = linkElement.attributes['href']!.split('=').last;
-
-      return (name: searchResult.name, url: link);
+      return (name: searchResult.name, url: url);
     } catch (e) {
       String message = '';
       if (e is ScrapeError) {
